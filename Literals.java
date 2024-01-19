689084d4faf04f2b75cd23a6a4e2bd363020d112
fardin36 <thunder.fardin@gmail.com>
2024-01-19 23:53:28 +0600

    Literals in Java

---
+++ Literals.java
@@ -0,0 +1,15 @@
+public class Literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 56666666666666666l;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Fardin";
+        System.out.println(str);
+    }
+}
