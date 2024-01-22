1b2905a1b7e7504df48ac2b87ebfa31f42b61f0c
fardin36 <thunder.fardin@gmail.com>
2024-01-22 15:00:19 +0600

    Chapter-1 Practice-1 half done

---
+++ Ch1_PS01.java
@@ -0,0 +1,16 @@
+public class Ch1_PS01 {
+  public static void main(String[] args) {
+    // Sum 3 numbers
+    int a = 55;
+    int b = 65;
+    int c = 75;
+    System.out.println(a + b + c);
+
+    int subject1 = 55;
+    int subject2 = 79;
+    int subject3 = 99;
+
+    float cgpa = (subject1 + subject2 + subject3) / 30.0f;
+    System.out.println("CGPA : " + cgpa);
+  }
+};
