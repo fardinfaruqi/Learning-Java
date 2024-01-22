a442cecb785d523fd7fdaf2ae61d31360cd72659
fardin36 <thunder.fardin@gmail.com>
2024-01-22 21:53:24 +0600

    Operators precedence in Java

---
+++ _009_op_pre.java
@@ -0,0 +1,25 @@
+public class _009_op_pre {
+    public static void main(String[] args) {
+        int a = 6 * 5 - 34 / 2;
+        System.out.println(a);
+    }
+}
+
+/*
+    Operator Precedencc     
+    () Function call
+    []
+    .
+    ->
+    ___________________
+    !
+    ~
+    -
+    ++
+    --
+    &
+    *
+    (typa)
+    sizeof
+    ___________________
+*/
