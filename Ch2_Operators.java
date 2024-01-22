9e99608add96b960bfbcaed7179a6575642f643f
fardin36 <thunder.fardin@gmail.com>
2024-01-22 21:35:03 +0600

    ch2_operators

---
+++ Ch2_Operators.java
@@ -0,0 +1,14 @@
+public class Ch2_Operators {
+    public static void main(String args[]) {
+        int a = 4;
+        int b = 6 + a;
+        System.out.println(b);
+        b = 6 % a; // Modular operator
+        System.out.println(b);
+        b += 5;
+        System.out.println(b);
+        System.out.println(8 == 6); // Comparison operator
+
+        System.out.println(2.3f % 1.3);
+    }
+}
