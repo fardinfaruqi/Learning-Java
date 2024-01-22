c225668c5322b7104d0fa1af43d9528a594446b0
fardin36 <thunder.fardin@gmail.com>
2024-01-22 14:11:04 +0600

    Problem to understand string input

--- _005_TakingInput.java
+++ _005_TakingInput.java
@@ -4,6 +4,7 @@ public class _005_TakingInput {
     public static void main(String[] args) {
         System.out.println("It works!");
         System.out.println("Taking input from the user---------");
+
         Scanner sc = new Scanner(System.in); // Scanner object
 
         System.out.print("Enter integer 1: ");
@@ -18,12 +19,18 @@ public class _005_TakingInput {
         float c = sc.nextFloat(); // taking float input
         System.out.println("Floating point you entered is " + c);
 
+        System.out.print("Integer validation: ");
         boolean b1 = sc.hasNextInt(); // taking boolean input for valid integer input
         System.out.println(b1);
 
+        System.out.print("Enter a word: ");
         String str = sc.next(); // taking single word string input
         System.out.println(str);
+
+        System.out.print("Enter an string: ");
         String str2 = sc.nextLine(); // taking one line string input
         System.out.println(str2);
+
+        sc.close();
     }
 }
