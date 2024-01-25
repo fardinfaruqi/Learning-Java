11f40e5ad8c3ea1974ded1ea1fded8f75d0fea6c
fardin36 <thunder.fardin@gmail.com>
2024-01-26 00:26:29 +0600

    For loop

---
+++ _023_ForLoop.java
@@ -0,0 +1,15 @@
+public class _023_ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i + ",");
+        }
+        System.out.println("");
+
+        for (int i = 10; i >= 0; i--) {
+            System.out.print(i + ",");
+        }
+        System.out.println("");
+
+    }
+}
