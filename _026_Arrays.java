6497703a0592f6f9923f6f3407def8c998b6b778
fardin36 <thunder.fardin@gmail.com>
2024-01-28 01:30:53 +0600

    arrays in java

---
+++ _026_Arrays.java
@@ -0,0 +1,14 @@
+public class _026_Arrays {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+
+        int arr2[];
+        arr2 = new int[5];
+
+        int[] arr3 = { 0, 1, 2, 3, 4 };
+
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.println(arr3[i]);
+        }
+    }
+}
