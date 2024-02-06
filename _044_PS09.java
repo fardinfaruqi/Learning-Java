43eb7efb803f9ced77b37ab9d3404f34c211b2a1
fardin36 <thunder.fardin@gmail.com>
2024-02-07 00:19:10 +0600

    Practice Problem

---
+++ _044_PS09.java
@@ -0,0 +1,50 @@
+public class _044_PS09 {
+    public static void main(String[] args) {
+        Cylinder mCylinder = new Cylinder(12, 9);
+
+        System.out.println(mCylinder.getRadius());
+        System.out.println(mCylinder.getHeight());
+
+        mCylinder.setHeight(12);
+        System.out.println(mCylinder.getHeight());
+        mCylinder.setRadius(9);
+        System.out.println(mCylinder.getRadius());
+
+        System.out.println(mCylinder.surfaceArea());
+        System.out.println(mCylinder.volume());
+    }
+}
+
+class Cylinder {
+    private int radius;
+    private int height;
+
+    public Cylinder(int a, int b) {
+        this.radius = a;
+        this.height = b;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public double surfaceArea() {
+        return ((2 * 3.1416 * (radius * height)) + (2 * 3.1416 * (radius * radius)));
+    }
+
+    public double volume() {
+        return (3.1416 * (radius * radius) * height);
+    }
+}
