117e61c98cd2e9d749fe3b49c14ee385847df0ee
fardin36 <bahauddinfaruqi@gmail.com>
2024-02-16 20:35:51 +0600

    Throw and throws in java

---
+++ _084_throw_throws.java
@@ -0,0 +1,41 @@
+public class _084_throw_throws {
+
+    public static double area(int r) throws NegativeRadiusException {
+        if (r < 0) {
+            throw new NegativeRadiusException();
+        }
+        double result = Math.PI * r * r;
+        return result;
+    }
+
+    public static int divide(int a, int b) throws ArithmeticException {
+        // Made By Fardin
+        int result = a / b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Salman - uses divide function created by Fardin
+        try {
+            // int c = divide(6, 0);
+            // System.out.println(c);
+            double ar = area(6);
+            System.out.println(ar);
+        } catch (Exception e) {
+            System.out.println("Exception");
+        }
+    }
+
+}
+
+class NegativeRadiusException extends Exception {
+    @Override
+    public String toString() {
+        return "Radius cannot be negative!";
+    }
+
+    @Override
+    public String getMessage() {
+        return "Radius cannot be negative!";
+    }
+}
