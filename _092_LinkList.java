83b04f865bf41ea075f68ce9f046132776dc23d1
fardin36 <bahauddinfaruqi@gmail.com>
2024-03-08 23:50:19 +0600

    LinkList in Java

---
+++ _092_LinkList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class _092_LinkList {
+    public static void main(String[] args) {
+        LinkedList<Integer> l1 = new LinkedList<>();
+        LinkedList<Integer> l2 = new LinkedList<>();
+        l1.add(6);
+        l1.add(7);
+        l1.add(4);
+        l1.add(6);
+        l1.add(5);
+        l2.add(14);
+        l2.add(15);
+        l2.add(16);
+        l2.add(17);
+        l2.add(18);
+        l1.addAll(l2);
+        System.out.println(l1.contains(1));
+        System.out.println(l1.contains(5));
+        System.out.println(l1.indexOf(5));
+        System.out.println(l1.indexOf(55));
+        System.out.println(l1.lastIndexOf(6));
+        for (int i = 0; i < l1.size(); i++) {
+            System.out.print(l1.get(i) + ", ");
+        }
+        System.out.println();
+        l1.add(0, 1);
+        for (int i = 0; i < l1.size(); i++) {
+            System.out.print(l1.get(i) + ", ");
+        }
+        l1.clear();
+        l2.clear();
+        for (int i = 0; i < l1.size(); i++) {
+            System.out.print(l1.get(i) + ", ");
+        }
+    }
+}
